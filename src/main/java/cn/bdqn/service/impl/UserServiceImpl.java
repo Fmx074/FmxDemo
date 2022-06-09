5c8d00305177cb8c3e7dcabdc8b6f55c0cfd4664
Fmx <Fmx_074@163.com>
2022-06-09 14:42:55 +0800

    第一个练习

--- src/main/java/cn/bdqn/service/impl/UserServiceImpl.java
+++ src/main/java/cn/bdqn/service/impl/UserServiceImpl.java
@@ -15,7 +15,7 @@ public class UserServiceImpl implements UserService {
     private UserMapper userMapper;
 
 //    @Override
-//    public List<UserPojoddasdafasdasdcal> list() {
+//    public List<UserPojoddasdafasdasdcal>asdasdasdadas list() {
 //        return userMapper.list();
 //    }
 
